7b171d89bd485be80295752c051a317085805d49
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2020-02-04 08:16:40 +0100

    Add files via upload

---
+++ parcoursSéquentiel.py
@@ -0,0 +1,29 @@
+import random
+
+maxVal = 20
+nVal = 10
+laClasse = [("Zoé", 17), ("Jed", 14),("Léo", 15), ("Noé", 16), ("Ana", 18), ("Max", 18), ("Lin", 17), ("Robert", 44)]
+#print(sorted(L,reverse=True))
+
+
+def comptage(L: list)-> int:
+    # A Completer
+    # ....
+    # ....
+    return # A Completer
+
+
+def preAdo(L: list)-> str:
+    # A Completer
+    # ....
+    # ....
+    return # A Completer
+
+def grosFreak(L: list)-> int:
+    # A Completer
+    # ....
+    # ....
+    return # A Completer
+
+
+print(comptage(laClasse))
