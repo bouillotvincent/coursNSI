7e28cb887a7758046a1273edbe108596a0396434
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2020-02-04 08:19:37 +0100

    Add files via upload

---
+++ rechercheÉlément.py
@@ -0,0 +1,19 @@
+import random
+
+maxVal = 20
+nVal = 10
+listeNombres = random.choices(range(maxVal),k=nVal)
+
+def rechercheNaive(L: list, elt: int)-> int:
+    # A Completer
+    # ....
+    # ....
+    return # A Completer
+
+def rechercheDicho(L: list, elt: int)-> int:
+    # A Completer
+    # ....
+    # ....
+    return # A Completer
+
+print(listeNombres)
