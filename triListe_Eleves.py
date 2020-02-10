162fe32bb48fabe521863518a504b9e6984f8d06
bouillotvincent <43422054+bouillotvincent@users.noreply.github.com>
2020-02-10 23:34:23 +0100

    Add files via upload

---
+++ triListe_Eleves.py
@@ -0,0 +1,23 @@
+import time
+import random
+
+maxVal = 10
+nVal = 5
+listeNombres = random.choices(range(maxVal),k=nVal)
+
+def triSelection(L: list)-> list:
+    """
+    A compléter
+    """
+    return # A compléter
+
+def triInsertion(L: list)-> list:
+    """
+    A compléter
+    """
+    return # A compléter
+
+
+listeNombresTriee = triSelection(listeNombres) # trie la liste
+
+print(listeNombresTriee) # affiche la liste
